18c67f80e959e1499de0ab0115c29fa446f6e05d
vikash kumar <119092991+vikashkumar12218021@users.noreply.github.com>
2023-01-09 11:44:01 +0530

    Add files via upload

---
+++ class1.update.py
@@ -0,0 +1,11 @@
+use_info = {
+    'name':'vikash',
+    'class': 12,
+    "Roll.no":12
+}
+more_info = {"name":"vikash kumar",
+    "age":33,
+    "section":"KOC31"
+}
+use_info.update(more_info)
+print(use_info)
